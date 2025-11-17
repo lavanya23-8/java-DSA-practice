c94383d46804f0cc373b9c329b4ceb7cc73294c4
LAVANYA B Y <lavanyalavanya0555@gmail.com>
2025-11-17 19:26:00 +0530

    Add files via upload

---
+++ Palindrome.java
@@ -0,0 +1,15 @@
+public class Palindrome {
+    public static void main(String[] args) {
+        int n = 121, temp = n, rev = 0;
+
+        while (n > 0) {
+            rev = rev * 10 + n % 10;
+            n /= 10;
+        }
+
+        if (rev == temp)
+            System.out.println("Palindrome");
+        else
+            System.out.println("Not Palindrome");
+    }
+}
