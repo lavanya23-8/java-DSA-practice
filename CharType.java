50c4afd8d685aa997324ce03a5cdfd112be3f07b
LAVANYA B Y <lavanyalavanya0555@gmail.com>
2025-11-25 20:22:23 +0530

    Add files via upload

---
+++ CharType.java
@@ -0,0 +1,12 @@
+public class CharType {
+    public static void main(String[] args) {
+        char ch = '#';
+
+        if (Character.isLetter(ch))
+            System.out.println("Alphabet");
+        else if (Character.isDigit(ch))
+            System.out.println("Digit");
+        else
+            System.out.println("Special Character");
+    }
+}
