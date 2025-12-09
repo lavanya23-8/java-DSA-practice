61a1c5b508b1d566343f1eef964a7749d1ceed8f
LAVANYA B Y <lavanyalavanya0555@gmail.com>
2025-12-09 19:04:47 +0530

    Add files via upload

---
+++ Remove Spaces From a String.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        String s = "Java is powerful";
+        String result = s.replace(" ", "");
+        System.out.println(result);
+    }
+}
