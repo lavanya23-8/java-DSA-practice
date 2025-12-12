f69cc3901de03a779046c9a8665d9b6224f416ca
LAVANYA B Y <lavanyalavanya0555@gmail.com>
2025-12-12 21:41:23 +0530

    Add files via upload

--- SecondLargest.java
+++ SecondLargest.java
@@ -1,18 +1,18 @@
 public class SecondLargest {
     public static void main(String[] args) {
-        int[] arr = {23, 89, 45, 12, 78, 90, 67};
-        int largest = Integer.MIN_VALUE;
-        int secondLargest = Integer.MIN_VALUE;
+        int arr[] = {12, 5, 8, 30, 25};
+
+        int first = Integer.MIN_VALUE, second = Integer.MIN_VALUE;
 
         for (int num : arr) {
-            if (num > largest) {
-                secondLargest = largest;
-                largest = num;
-            } else if (num > secondLargest && num != largest) {
-                secondLargest = num;
+            if (num > first) {
+                second = first;
+                first = num;
+            } else if (num > second && num != first) {
+                second = num;
             }
         }
 
-        System.out.println("Second Largest Number: " + secondLargest);
+        System.out.println("Second Largest: " + second);
     }
 }
