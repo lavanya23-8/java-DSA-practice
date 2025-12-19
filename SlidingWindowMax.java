36f7f867a63e6a4c80239fe2d59f0b78057d8e9c
LAVANYA B Y <lavanyalavanya0555@gmail.com>
2025-12-19 19:24:09 +0530

    Add files via upload

---
+++ SlidingWindowMax.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class SlidingWindowMax {
+    public static int[] maxSlidingWindow(int[] nums, int k) {
+        Deque<Integer> dq = new ArrayDeque<>();
+        int[] res = new int[nums.length - k + 1];
+        int idx = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            while (!dq.isEmpty() && dq.peekFirst() <= i - k) dq.pollFirst();
+            while (!dq.isEmpty() && nums[dq.peekLast()] <= nums[i]) dq.pollLast();
+            dq.offerLast(i);
+            if (i >= k - 1) res[idx++] = nums[dq.peekFirst()];
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(
+            maxSlidingWindow(new int[]{1,3,-1,-3,5,3,6,7}, 3)));
+    }
+}
